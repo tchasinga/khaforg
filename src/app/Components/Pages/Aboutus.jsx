800c08c7e8eeff7e98b7c51b51b7b320bc77aa08
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-16 16:52:27 +0300

    Refactor Aboutus component: standardize quotes and clean up formatting

--- src/app/Components/Pages/Aboutus.jsx
+++ src/app/Components/Pages/Aboutus.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
+import React from "react";
 import TypingAnimation from "../../animation/typing-animation.tsx";
 
 export default function Aboutus() {
-
     
-
   return (
-    <div className='py-[3%] max-w-screen-xl mx-auto'>
-        <div className='text-3xl text-blue-600 font-bold'>
-             <h1 className='font-bold'>
-             <TypingAnimation
-      text="Typing Animation"
-    />
-             </h1>
-        </div>
+    <div className="py-[3%] max-w-screen-xl mx-auto">
+      <div className="text-3xl text-blue-600 font-bold">
+        <h1 className="font-bold">
+          <TypingAnimation text="Typing Animation" />
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
