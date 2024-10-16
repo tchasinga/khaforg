df695fc889ab46229f1f6a37a3890fc4ad315bd3
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-16 11:23:37 +0300

    Add swiper dependency and implement global styles; create Hero component and update layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Header from "./Components/Header";
+import Hero from "./Components/Pages/Hero";
 
 export default function Home() {
   return (
     <div className="">
-  
+      <Header />
+      <Hero/> 
     </div>
   );
 }
