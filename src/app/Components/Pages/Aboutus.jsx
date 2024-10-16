fd0a9ca1ecec94ec091a329ff9330e51eb5bdb1b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-16 17:26:35 +0300

    Add horizontal rule and comment in Aboutus component: enhance layout and clarify code

--- src/app/Components/Pages/Aboutus.jsx
+++ src/app/Components/Pages/Aboutus.jsx
@@ -8,7 +8,10 @@ export default function Aboutus() {
     <div className="py-[3%] max-w-screen-xl mx-auto">
       <div className="text-3xl text-blue-600 font-bold">
           <TypingAnimation text="learn more about us" />
+          <hr className="w-20"/>
       </div>
+
+      {/* my flex function is addded now is the containant */}
     </div>
   );
 }
