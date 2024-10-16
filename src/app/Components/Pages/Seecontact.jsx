908d005167fe92268cdb0792638c885c0de50791
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-16 10:14:46 +0300

    Update Seecontact component: add background color to enhance visibility

--- src/app/Components/Pages/Seecontact.jsx
+++ src/app/Components/Pages/Seecontact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Seecontact() {
   return (
-    <div className='flex flex-col items-center justify-center w-full h-screen'>
+    <div className='flex flex-col items-center justify-center w-full h-screen bg-black opacity-100'>
         <div className="">
         </div>
     </div>
