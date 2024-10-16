6589f9c653206982d07e8b6d623b044a14a6b5ed
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-16 16:52:14 +0300

    Add TypingAnimation to Aboutus component: integrate typing effect in header

--- src/app/Components/Pages/Aboutus.jsx
+++ src/app/Components/Pages/Aboutus.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TypingAnimation from "../../animation/typing-animation.tsx";
 
 export default function Aboutus() {
 
@@ -7,7 +8,11 @@ export default function Aboutus() {
   return (
     <div className='py-[3%] max-w-screen-xl mx-auto'>
         <div className='text-3xl text-blue-600 font-bold'>
-             <h1 className='font-bold'>LEARN ABOUT US</h1>
+             <h1 className='font-bold'>
+             <TypingAnimation
+      text="Typing Animation"
+    />
+             </h1>
         </div>
     </div>
   )
