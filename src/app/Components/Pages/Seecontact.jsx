b17ffff7079a511f4a06e3f9f1263cb7d1184ff1
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-16 10:17:49 +0300

    Update Seecontact component: enhance layout with background and add placeholder heading; create firstContact data file

--- src/app/Components/Pages/Seecontact.jsx
+++ src/app/Components/Pages/Seecontact.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Seecontact() {
   return (
     <div className='flex flex-col items-center justify-center w-full h-screen bg-black opacity-100'>
-        <div className="">
+        <div className="max-w-md w-full bg-white p-5 rounded-xl">
+            <h1>balole</h1>
         </div>
     </div>
   )
