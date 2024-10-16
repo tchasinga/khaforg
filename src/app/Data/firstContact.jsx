193afd6da4aada2f8575abe50b07e850b12213ca
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-16 10:23:16 +0300

    Add firstContact data file with emergency contact details and icons

--- src/app/Data/firstContact.jsx
+++ src/app/Data/firstContact.jsx
@@ -1,3 +1,21 @@
+import { IoCallOutline } from "react-icons/io5";
+import { MdOutlineMail } from "react-icons/md";
+import { MdOutlineEventAvailable } from "react-icons/md";
+
 const firstContact = [
-    
+  {
+    id: 1,
+    name: "Emergency",
+    icons : <IoCallOutline />
+  },
+  {
+    id: 2,
+    name: "Email: reception@chan-f.or.ke",
+    icons : <MdOutlineMail />
+  },
+  {
+    id: 3,
+    name: "Opening Hours || Monday – Sunday: 24hrs",
+    icons : <MdOutlineEventAvailable />
+  }
 ]
