86a96e7bd3ffa3366ca8e11b8ee8dbceceb98b3f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-16 10:44:14 +0300

    Update Seecontact component: integrate firstContact data and enhance layout with grid display

--- src/app/Components/Pages/Seecontact.jsx
+++ src/app/Components/Pages/Seecontact.jsx
@@ -1,10 +1,26 @@
+import firstContact from '@/app/Data/firstContact.js'
 import React from 'react'
 
 export default function Seecontact() {
   return (
     <div className='flex flex-col items-center justify-center w-full h-screen bg-black opacity-100'>
         <div className="max-w-md w-full bg-white p-5 rounded-xl">
-            
+            <div className="mygridtemplate">
+                {
+                    firstContact.map((item) => (
+                     <>
+                       <div className="flex items-center gap-3 p-3 border-2 border-gray-400 rounded-xl">
+                       <div className='flex items-center gap-3 text-2xl text-blue-600 font-bold'>
+                            {item.icons}
+                        </div>
+                         <div className="w-full">
+                            <h1 className='text-xs  w-full'>{item.name}</h1>
+                         </div>
+                       </div>
+                     </>
+                    ))
+                }
+            </div>
         </div>
     </div>
   )
