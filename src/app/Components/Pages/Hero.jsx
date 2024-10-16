625ceddd1b5edd78ddbcb356dab1f2f776f28b97
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-16 12:25:38 +0300

    Update Header and Hero components: enhance dropdown menu z-index and add multiple image URLs for Swiper

--- src/app/Components/Pages/Hero.jsx
+++ src/app/Components/Pages/Hero.jsx
@@ -1,31 +1,47 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
 import SwiperCore from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function Hero() {
-
-    SwiperCore.use([Navigation]);
-    const imageUrls = [
-        "",
-    ]
+  SwiperCore.use([Navigation]);
+  const imageUrls = [
+    "https://images.pexels.com/photos/356040/pexels-photo-356040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/863977/pexels-photo-863977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/6516209/pexels-photo-6516209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/25599838/pexels-photo-25599838/free-photo-of-woman-sitting-and-exercising.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/8769168/pexels-photo-8769168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3931116/pexels-photo-3931116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3758124/pexels-photo-3758124.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/14921377/pexels-photo-14921377.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/9376270/pexels-photo-9376270.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/11869770/pexels-photo-11869770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/2294363/pexels-photo-2294363.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/105028/pexels-photo-105028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
 
   return (
     <div>
-       <Swiper navigation>
-            {imageUrls.map((imagurl) => (
-              <SwiperSlide key={imagurl}>
-                <div className="h-[550px] relative object-cover" style={{background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${imagurl}) center no-repeat`, backgroundSize: "cover",}}>
-                  {/* <div className="text-white text-4xl absolute bottom-10 right-0 left-0 px-5 font-bold">
+      <Swiper navigation>
+        {imageUrls.map((imagurl) => (
+          <SwiperSlide key={imagurl}>
+            <div
+              className="h-[550px] relative object-cover"
+              style={{
+                background: `linear-gradient(to right, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${imagurl}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            >
+              {/* <div className="text-white text-4xl absolute bottom-10 right-0 left-0 px-5 font-bold">
                      <h1>{sharing.publicrole}</h1>
                      <h3 className="text-xs text-gray-500">{sharing.email}</h3>
                   </div> */}
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
+  );
 }
