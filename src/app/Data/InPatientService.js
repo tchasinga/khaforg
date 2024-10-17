1e9c7d137b8681f169add337651f66161017061c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-17 18:13:53 +0300

    Add InPatientService data with icons and titles for healthcare services

---
+++ src/app/Data/InPatientService.js
@@ -0,0 +1,33 @@
+import { FaHandHoldingMedical } from "react-icons/fa";
+import { BsPersonArmsUp } from "react-icons/bs";
+import { FaBaby } from "react-icons/fa";
+import { FaBriefcaseMedical } from "react-icons/fa6";
+import { FaBedPulse } from "react-icons/fa6";
+
+const InPatientService = [
+    {
+        id : 1,
+        icons : <FaHandHoldingMedical />,
+        title : "Operation Theatre",
+    },
+    {
+        id: 2,
+        icons : <BsPersonArmsUp />,
+        title : "Paeditric Care",
+    },
+    {
+        id : 3,
+        icons : <FaBaby />,
+        title : "Maternity Care",
+    },
+    {
+        id: 4,
+        icons : <FaBriefcaseMedical />,
+        title: "Critical care",
+    },
+    {
+        id: 5,
+        icons : <FaBedPulse />,
+        title : "Adult ward"
+    }
+]
